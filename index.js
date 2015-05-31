506f285a8bb31ef58b0e2dfd6dfbc1b730835dca
y3seker <y3seker@gmail.com>
2015-06-01 01:47:22 +0300

    fix for /kayit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var ip = (process.env.OPENSHIFT_NODEJS_IP === undefined ? '127.0.0.1' : process.
 process.env.TZ = 'Europe/Istanbul';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 
 module.exports.urlencodedParser = bodyParser.urlencoded({
